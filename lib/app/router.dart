faa248ba1e4de459631a3ba9b87ef0d66c4bbbb8
MohamedAbdallah-14 <mohamedmabdallah14@gmail.com>
2023-02-14 20:24:13 +0200

    [Router] News List

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/home/view/router.dart';
 import 'package:news_app/home/view/splash.dart';
 
 class AppRouter {
   AppRouter._();
   static Route<Widget> onGenerateRoute(RouteSettings routeSettings) {
-    return MaterialPageRoute(builder: (_) => const Splash());
+    return HomeRouter.onGenerateRoute(routeSettings) ??
+        MaterialPageRoute(builder: (_) => const Splash());
   }
 }
---
+++ lib/home/view/router.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeRouter {
+  HomeRouter._();
+  static Route<Widget>? onGenerateRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      default:
+        return null;
+    }
+  }
+}
