6f053bdbbd0197dff29b10571bc46e72129d4d01
MohamedAbdallah-14 <mohamedmabdallah14@gmail.com>
2023-02-14 21:31:49 +0200

    GenericErrorHandler

--- lib/helpers/app_colors.dart
+++ lib/helpers/app_colors.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+  static const primary = darkBlueGrey;
+  static const white = Colors.white;
   static const darkBlueGrey = Color(0xff0f1737);
   static const black40 = Color(0x66000000);
+
+  static const brownishGrey = Color(0xff747474);
+  static const paleGrey = Color(0xfff3f4f7);
+  static const darkGreyBlue = Color(0xff325268);
 }
