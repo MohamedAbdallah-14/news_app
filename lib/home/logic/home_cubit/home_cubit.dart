1594b2293eaac4636bd910840630a83426eb127c
MohamedAbdallah-14 <mohamedmabdallah14@gmail.com>
2023-02-15 14:44:42 +0200

    [Logic] Home Navigation Cubit

---
+++ lib/home/logic/home_cubit/home_cubit.dart
@@ -0,0 +1,17 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:news_app/home/view/home_screen/tabs/home_tab.dart';
+import 'package:news_app/home/view/home_screen/tabs/news_tab.dart';
+import 'package:news_app/home/view/home_screen/tabs/not_implemented_tab.dart';
+import 'package:news_app/home/view/home_screen/tabs_enum.dart';
+
+part 'home_state.dart';
+
+class HomeCubit extends Cubit<HomeState> {
+  HomeCubit() : super(const HomeState(currentTab: NavigationTabs.home));
+
+  void changeTab(NavigationTabs tab) {
+    emit(HomeState(currentTab: tab));
+  }
+}
